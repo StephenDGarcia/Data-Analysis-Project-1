24e2d0802773cd2017c8bbc7031ed1797d61b098
StephenDGarcia <stephendgarcia@gmail.com>
2022-01-25 13:38:46 -0600

    Missing break after trying again
    When repeating I'm not getting the option to stop playing. Need more scores to get the math from the list.

--- guessing_game.py
+++ guessing_game.py
@@ -3,15 +3,25 @@ Python Web Development Techdegree
 Project 1 - Number Guessing Game
 """
 import random
+
+score_list = []
+
+def add_to_list(count):
+    score_list.append(count)
+
 def start_game():
     answer = random.randint(1,100)
-    print(answer)
+    #print(answer)
     count = 0    
     print("Welcome to my Guessing Game")
     while True:
         guessed_number = input("Enter a number between 1 and 100:  ")
         try:
             guessed_number = int(guessed_number)
+            if guessed_number > 100:
+                raise ValueError
+            elif guessed_number < 0:
+                raise ValueError
         except ValueError:
             print("Please enter a number")
         else:
@@ -21,14 +31,18 @@ def start_game():
             elif guessed_number < answer:
                 print("It's higher, try again")
             else:
-                print("Got it! It took you {} tries! \n GAME OVER".format(count))
+                print("Got it! It took you {} tries! \n Mean: \n Median: \n Mode:".format(count))
+                add_to_list(count)
                 break
 start_game()
 
 repeat = input("Would you like to play again Y/N?  ")
 while True:
-    if repeat == "N":
-            break
-    elif repeat == "Y":
+    if repeat == "Y":
         start_game()
+    else:
+        repeat == "N"
+        print("GAME OVER, GOOD BYE")
+        break
+    
         
